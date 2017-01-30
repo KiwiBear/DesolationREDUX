8e51a8c903764b1b1524c0eeb13fb5f590d27479
Legodev <legodevgit@mailbox.org>
2017-01-30 10:39:20 -0500

    readded deleted files

---
+++ Client Files/@Desolation/Addons/dsr_vehicles/config.cpp
@@ -0,0 +1,17 @@
+class CfgPatches
+{
+	class dsr_vehicles
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.04;
+		requiredAddons[] = {"A3_Data_F"};
+	};
+};
+class CfgEditorSubcategories
+{
+	class BikeSubcategory // Category class, you point to it in editorSubcategory property
+	{
+		displayName = "Bicycle"; // Name visible in the list
+	};
+};
